07760aa604d1d8ef5532c4beb65103c55360ac48
Renad <nohaibrahim@mail.com>
2021-11-19 01:33:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+function add4strings(str1,str2,str3,str4){
+    return str1+str2+str3+str4;
+} 
+console.log("ahmed".concat("ali","hoda","hrdr","lpoi"));
+var str="welcome mr ahmed"
+console.log(str.slice(10,16)); 
+console.log(str.substr(10,16));
+console.log(str.charAt(10));
+var str1="ahmed"
+str2=str1.replace("ahmed","mohamed"); 
+console.log(str2);
+str2=str1.toUpperCase();
+console.log(str2);
+str2=str1.toLowerCase();
+console.log(str2);
+console.log(str.includes("yasser"));
+
+
+var num="10.987654";
+console.log(parseInt(num));
+console.log(parseFloat(num));  
+var num = 6.8676;
+console.log(num.toFixed());
+console.log(num.toFixed(1));
